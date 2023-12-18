5fae100dfe85b93e9d3a95950ddb9920b7b87c67
StefPel <stephaniepelouin@gmail.com>
2023-12-18 14:17:41 +0100

    Étape 4
    Modifiez le slide au clic sur le bouton

--- assets/script.js
+++ assets/script.js
@@ -17,24 +17,64 @@ const slides = [
 	}
 ]
 
-/********** variables *********/
-let dots = document.querySelector(".dots");	
+// Sélection des éléments du DOM
+let dots = document.querySelector(".dots"); // Sélectionne le conteneur des points de navigation
 
-/******** carrousel **********/
+// Fonction pour passer à l'image suivante
+function next() {
+    if (img.dataset.index < slides.length - 1) { // Si ce n'est pas la dernière image
+        img.dataset.index++; // Passe à l'image suivante
+        // Met à jour les points de navigation
+        document.querySelector(".dot_selected").nextElementSibling.classList.add("dot_selected");
+        document.querySelector(".dot_selected").classList.remove("dot_selected");
+        // Change l'image et le texte
+        img.setAttribute("src", "./assets/images/slideshow/" + slides[img.dataset.index].image);
+        img.nextElementSibling.innerHTML = slides[img.dataset.index].tagLine;
+    } else { // Si c'est la dernière image
+        img.dataset.index = 0; // Retourne à la première image
+        // Réinitialise les points de navigation et l'image
+        img.setAttribute("src", "./assets/images/slideshow/" + slides[0].image);
+        img.nextElementSibling.innerHTML = slides[0].tagLine;
+        dots.firstElementChild.classList.add("dot_selected");
+        dots.lastElementChild.classList.remove("dot_selected");
+    }
+}
 
-for(let i = 0; i < slides.length; i++){
-	const content = document.createElement("span");
-	content.classList.add("dot")
-	dots.appendChild(content);
-	dots.firstElementChild.classList.add("dot_selected");	
-};
+// Fonction pour revenir à l'image précédente
+function previous() {
+    if (img.dataset.index > 0) { // Si ce n'est pas la première image
+        img.dataset.index--; // Retourne à l'image précédente
+        // Met à jour les points de navigation
+        document.querySelector(".dot_selected").previousElementSibling.classList.add("dot_selected");
+        document.querySelector(".dot_selected").classList.remove("dot_selected");
+        // Change l'image et le texte
+        img.setAttribute("src", "./assets/images/slideshow/" + slides[img.dataset.index].image);
+        img.nextElementSibling.innerHTML = slides[img.dataset.index].tagLine;
+    } else { // Si c'est la première image
+        img.dataset.index = slides.length - 1; // Va à la dernière image
+        // Réinitialise les points de navigation et l'image
+        img.setAttribute("src", "./assets/images/slideshow/" + slides[slides.length - 1].image);
+        img.nextElementSibling.innerHTML = slides[slides.length - 1].tagLine;
+        dots.lastElementChild.classList.add("dot_selected");
+        dots.firstElementChild.classList.remove("dot_selected");
+    }
+}
 
-arrowRight.addEventListener("click", function(){
-	console.log("suivant");	
-	next();
+// Initialisation du carrousel
+for (let i = 0; i < slides.length; i++) {
+    const content = document.createElement("span");
+    content.classList.add("dot");
+    dots.appendChild(content);
+}
+dots.firstElementChild.classList.add("dot_selected");
+
+// Gestion des événements de clic sur les flèches
+arrowRight.addEventListener("click", function () {
+    console.log("suivant");
+    next();
 });
 
-arrowLeft.addEventListener("click", function(){
-	console.log("précédent");
-	previous();
+arrowLeft.addEventListener("click", function () {
+    console.log("précédent");
+    previous();
 });
